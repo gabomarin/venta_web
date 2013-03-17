f08539df02bbb772ca3702d834351874e45a7263
lexds_1591 <lexds_1591_1591@hotmail.com>
2013-03-17 23:43:05 +0000

    "usuario" and "producto" were added

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-	echo 'Hello World';
+
+/**
+ * @package mvc
+ */
+
+//Creamos el contralador en base a lo requerido y lo mandamos a ejecutar
+switch($_REQUEST['modulo'])
+{
+	case 'usuario': 
+		include('Controller/usuarioCtl.php');
+		$controlador = new usuarioCtl();
+		break;
+	case 'producto' : 
+		include('Controller/productoCtl.php');
+		$controlador = new productoCtl(); 
+		break;
+}
+
+//Ejecutamos el controlador
+$controlador -> ejecutar();
+
 ?>
