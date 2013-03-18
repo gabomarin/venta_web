f989ea7b54c285dfeecc0fde45c83a45edd14686
lexds_1591 <lexds_1591_1591@hotmail.com>
2013-03-18 01:34:51 +0000

    created, this file shows what was updated

---
+++ View/inventarioInsertadoView.php
@@ -0,0 +1,11 @@
+<?php
+
+echo 'inventario insertado con el id: '.$inventario->id;
+
+	echo $inventario-> cantidadProducto;
+	echo $inventario-> fecha;
+	echo $inventario-> cantidadReal;
+	echo $inventario-> cantidadEsperada;
+	echo $inventario-> descripcion;
+
+?>
