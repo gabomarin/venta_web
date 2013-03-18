41eab9865748dac3a2349da705ba51352564e201
lexds_1591 <lexds_1591_1591@hotmail.com>
2013-03-18 00:05:00 +0000

    created, this file shows what was updated

---
+++ View/usuarioInsertadoView.php
@@ -0,0 +1,14 @@
+<?php
+
+echo 'Usuario insertado con el id: '.$usuario->id;
+
+echo $usuario->nombre;
+echo $usuario->mail;
+echo $usuario->pass;
+echo $usuario->direccion;
+echo $usuario->rfc;
+echo $usuario->telefono;
+echo $usuario->estatus;
+echo $usuario->tipo;
+
+?>
