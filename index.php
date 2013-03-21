124d5804e26e1738d1878e858947b3dbfaabe3a5
gabriel <gabrielortiz_26@hotmail.com>
2013-03-20 20:59:57 -0600

    Fixed index.php default controller

--- index.php
+++ index.php
@@ -5,42 +5,50 @@
  */
 
 //Creamos el contralador en base a lo requerido y lo mandamos a ejecutar
-switch($_REQUEST['modulo'])
-{
-	case 'usuario': 
-		include('Controller/usuarioCtl.php');
-		$controlador = new usuarioCtl();
-		break;
-	case 'producto' : 
-		include('Controller/productoCtl.php');
-		$controlador = new productoCtl(); 
-		break;
-	case 'inventario':
-		include('Controller/inventarioCtl.php');
-		$controlador = new inventarioCtl();
-		break;
-		
-	case 'venta':
-		include('Controller/ventaCtl.php');
-		$controlador = new ventaCtl();
-		break;
-	case 'factura':
-		include('Controller/facturaCtl.php');
-		$controlador = new facturaCtl();
-		break;
-	
-	case 'estandar':
-		include('Controller/stdCtl.php');
-		$controlador= new stdCtl();
-		break;
-		
-	default://carga el controlador estandar
-			include("Controller/stdCtl.php");
+if (isset($_GET['modulo'])) {
+
+	switch($_REQUEST['modulo']) {
+		case 'usuario' :
+			include ('Controller/usuarioCtl.php');
+			$controlador = new usuarioCtl();
+			break;
+		case 'producto' :
+			include ('Controller/productoCtl.php');
+			$controlador = new productoCtl();
+			break;
+		case 'inventario' :
+			include ('Controller/inventarioCtl.php');
+			$controlador = new inventarioCtl();
+			break;
+
+		case 'venta' :
+			include ('Controller/ventaCtl.php');
+			$controlador = new ventaCtl();
+			break;
+		case 'factura' :
+			include ('Controller/facturaCtl.php');
+			$controlador = new facturaCtl();
+			break;
+
+		case 'estandar' :
+			include ('Controller/stdCtl.php');
 			$controlador = new stdCtl();
 			break;
-}
 
+		default :
+			//carga el controlador estandar
+			include ("Controller/stdCtl.php");
+			$controlador = new stdCtl();
+			break;
+	}
+
+	
+	
+} else {
+	include ("Controller/stdCtl.php");
+	$controlador = new stdCtl();
+	
+}
 //Ejecutamos el controlador
 $controlador -> ejecutar();
-
 ?>
