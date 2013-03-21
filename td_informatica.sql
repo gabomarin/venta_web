aff557ab4148c3c9ebbcecb5807f02861ec597cc
gabriel <gabrielortiz_26@hotmail.com>
2013-03-20 21:07:30 -0600

    Updated project database, with some inserts now

--- td_informatica.sql
+++ td_informatica.sql
@@ -1,166 +1,206 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- phpMyAdmin SQL Dump
+-- version 3.3.7deb7
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 20-03-2013 a las 22:03:24
+-- Versión del servidor: 5.1.66
+-- Versión de PHP: 5.3.3-7+squeeze15
 
-DROP SCHEMA IF EXISTS `cc409_virtualtd` ;
-CREATE SCHEMA IF NOT EXISTS `cc409_virtualtd` DEFAULT CHARACTER SET utf8 ;
-USE `cc409_virtualtd` ;
-
--- -----------------------------------------------------
--- Table `cc409_virtualtd`.`factura`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `cc409_virtualtd`.`factura` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `fecha` DATE NOT NULL ,
-  `precio` DOUBLE NOT NULL ,
-  `estatus` INT(11) NOT NULL ,
-  `cantidad` INT(11) NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = MyISAM
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = latin1;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
--- -----------------------------------------------------
--- Table `cc409_virtualtd`.`usuario`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `cc409_virtualtd`.`usuario` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `nombre` VARCHAR(45) NOT NULL ,
-  `mail` VARCHAR(40) NOT NULL ,
-  `pass` VARCHAR(45) NOT NULL ,
-  `rfc` VARCHAR(45) NULL DEFAULT NULL ,
-  `direccion` VARCHAR(45) NOT NULL ,
-  `telefono` VARCHAR(45) NOT NULL ,
-  `estatus` VARCHAR(45) NOT NULL ,
-  `tipo` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = latin1;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `cc409_virtualtd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_venta`
+--
+
+CREATE TABLE IF NOT EXISTS `detalle_venta` (
+  `producto_id` int(11) NOT NULL,
+  `venta_id` int(11) NOT NULL,
+  `total` double NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  PRIMARY KEY (`producto_id`,`venta_id`),
+  KEY `fk_producto_has_venta_venta1_idx` (`venta_id`),
+  KEY `fk_producto_has_venta_producto_idx` (`producto_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcar la base de datos para la tabla `detalle_venta`
+--
 
 
--- -----------------------------------------------------
--- Table `cc409_virtualtd`.`inventario`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `cc409_virtualtd`.`inventario` (
-  `id` INT(11) NOT NULL ,
-  `fecha` DATE NOT NULL ,
-  `cantidadProducto` INT(11) NOT NULL ,
-  `cantidadReal` INT(11) NOT NULL ,
-  `cantidadEsperada` INT(11) NOT NULL ,
-  `descripcion` VARCHAR(300) NOT NULL ,
-  `usuario_id` INT(11) NOT NULL ,
-  PRIMARY KEY (`id`, `usuario_id`) ,
-  CONSTRAINT `fk_inventario_usuario1`
-    FOREIGN KEY (`usuario_id` )
-    REFERENCES `cc409_virtualtd`.`usuario` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+-- --------------------------------------------------------
 
-CREATE INDEX `fk_inventario_usuario1_idx` ON `cc409_virtualtd`.`inventario` (`usuario_id` ASC) ;
+--
+-- Estructura de tabla para la tabla `factura`
+--
 
+CREATE TABLE IF NOT EXISTS `factura` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `precio` double NOT NULL,
+  `estatus` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
--- -----------------------------------------------------
--- Table `cc409_virtualtd`.`producto`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `cc409_virtualtd`.`producto` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `nombre` VARCHAR(45) NOT NULL ,
-  `estatus` INT(11) NOT NULL ,
-  `precio` DOUBLE NOT NULL ,
-  `existencia` INT(11) NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = latin1;
+--
+-- Volcar la base de datos para la tabla `factura`
+--
 
+INSERT INTO `factura` (`id`, `fecha`, `precio`, `estatus`, `cantidad`) VALUES
+(1, '2013-03-05', 12, 1, 12);
 
--- -----------------------------------------------------
--- Table `cc409_virtualtd`.`venta`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `cc409_virtualtd`.`venta` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `fecha` DATE NOT NULL ,
-  `total` DOUBLE NOT NULL ,
-  `factura_id` INT(11) NOT NULL ,
-  `usuario_id` INT(11) NOT NULL ,
-  PRIMARY KEY (`id`, `factura_id`, `usuario_id`) ,
-  CONSTRAINT `fk_venta_factura1`
-    FOREIGN KEY (`factura_id` )
-    REFERENCES `cc409_virtualtd`.`factura` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_venta_usuario1`
-    FOREIGN KEY (`usuario_id` )
-    REFERENCES `cc409_virtualtd`.`usuario` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = latin1;
+-- --------------------------------------------------------
 
-CREATE INDEX `fk_venta_factura1_idx` ON `cc409_virtualtd`.`venta` (`factura_id` ASC) ;
+--
+-- Estructura de tabla para la tabla `inventario`
+--
 
-CREATE INDEX `fk_venta_usuario1_idx` ON `cc409_virtualtd`.`venta` (`usuario_id` ASC) ;
+CREATE TABLE IF NOT EXISTS `inventario` (
+  `id` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `cantidadProducto` int(11) NOT NULL,
+  `cantidadReal` int(11) NOT NULL,
+  `cantidadEsperada` int(11) NOT NULL,
+  `descripcion` varchar(300) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`usuario_id`),
+  KEY `fk_inventario_usuario1_idx` (`usuario_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcar la base de datos para la tabla `inventario`
+--
 
 
--- -----------------------------------------------------
--- Table `cc409_virtualtd`.`detalle_venta`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `cc409_virtualtd`.`detalle_venta` (
-  `producto_id` INT(11) NOT NULL ,
-  `venta_id` INT(11) NOT NULL ,
-  `total` DOUBLE NOT NULL ,
-  `cantidad` INT NOT NULL ,
-  PRIMARY KEY (`producto_id`, `venta_id`) ,
-  CONSTRAINT `fk_producto_has_venta_producto`
-    FOREIGN KEY (`producto_id` )
-    REFERENCES `cc409_virtualtd`.`producto` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_producto_has_venta_venta1`
-    FOREIGN KEY (`venta_id` )
-    REFERENCES `cc409_virtualtd`.`venta` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+-- --------------------------------------------------------
 
-CREATE INDEX `fk_producto_has_venta_venta1_idx` ON `cc409_virtualtd`.`detalle_venta` (`venta_id` ASC) ;
+--
+-- Estructura de tabla para la tabla `inventario_has_producto`
+--
 
-CREATE INDEX `fk_producto_has_venta_producto_idx` ON `cc409_virtualtd`.`detalle_venta` (`producto_id` ASC) ;
+CREATE TABLE IF NOT EXISTS `inventario_has_producto` (
+  `inventario_id` int(11) NOT NULL,
+  `producto_id` int(11) NOT NULL,
+  PRIMARY KEY (`inventario_id`,`producto_id`),
+  KEY `fk_inventario_has_producto_producto1_idx` (`producto_id`),
+  KEY `fk_inventario_has_producto_inventario1_idx` (`inventario_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcar la base de datos para la tabla `inventario_has_producto`
+--
 
 
--- -----------------------------------------------------
--- Table `cc409_virtualtd`.`inventario_has_producto`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `cc409_virtualtd`.`inventario_has_producto` (
-  `inventario_id` INT(11) NOT NULL ,
-  `producto_id` INT(11) NOT NULL ,
-  PRIMARY KEY (`inventario_id`, `producto_id`) ,
-  CONSTRAINT `fk_inventario_has_producto_inventario1`
-    FOREIGN KEY (`inventario_id` )
-    REFERENCES `cc409_virtualtd`.`inventario` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_inventario_has_producto_producto1`
-    FOREIGN KEY (`producto_id` )
-    REFERENCES `cc409_virtualtd`.`producto` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+-- --------------------------------------------------------
 
-CREATE INDEX `fk_inventario_has_producto_producto1_idx` ON `cc409_virtualtd`.`inventario_has_producto` (`producto_id` ASC) ;
+--
+-- Estructura de tabla para la tabla `producto`
+--
 
-CREATE INDEX `fk_inventario_has_producto_inventario1_idx` ON `cc409_virtualtd`.`inventario_has_producto` (`inventario_id` ASC) ;
+CREATE TABLE IF NOT EXISTS `producto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `estatus` int(11) NOT NULL,
+  `precio` double NOT NULL,
+  `existencia` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
-USE `cc409_virtualtd` ;
+--
+-- Volcar la base de datos para la tabla `producto`
+--
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `mail` varchar(40) NOT NULL,
+  `pass` varchar(45) NOT NULL,
+  `rfc` varchar(45) DEFAULT NULL,
+  `direccion` varchar(45) NOT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `estatus` int(11) NOT NULL,
+  `tipo` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Volcar la base de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nombre`, `mail`, `pass`, `rfc`, `direccion`, `telefono`, `estatus`, `tipo`) VALUES
+(1, 'jesus', 'jesus@gmail.com', '1234', NULL, 'paseos del bosque 1282', '3312509523', 1, 'empleado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE IF NOT EXISTS `venta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `total` double NOT NULL,
+  `factura_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_venta_factura1_idx` (`factura_id`),
+  KEY `fk_venta_usuario1_idx` (`usuario_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Volcar la base de datos para la tabla `venta`
+--
+
+INSERT INTO `venta` (`id`, `fecha`, `total`, `factura_id`, `usuario_id`) VALUES
+(1, '2013-03-13', 300, 1, 1);
+
+--
+-- Filtros para las tablas descargadas (dump)
+--
+
+--
+-- Filtros para la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD CONSTRAINT `fk_producto_has_venta_producto` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_producto_has_venta_venta1` FOREIGN KEY (`venta_id`) REFERENCES `venta` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD CONSTRAINT `fk_inventario_usuario1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `inventario_has_producto`
+--
+ALTER TABLE `inventario_has_producto`
+  ADD CONSTRAINT `fk_inventario_has_producto_inventario1` FOREIGN KEY (`inventario_id`) REFERENCES `inventario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_inventario_has_producto_producto1` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_ibfk_3` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`factura_id`) REFERENCES `factura` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
