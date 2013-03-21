be7dc447ce61adb18d6e38ffc21d75f8f2851a00
gabriel <gabrielortiz_26@hotmail.com>
2013-03-20 20:47:04 -0600

    Created login model

---
+++ Model/loginBss.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @package mvc
+ * @subpackage model
+ */
+
+class loginBss{
+	
+	/**
+	 * @param string $nombre
+	 * @param string $mail
+	 * @param string $pass encriptado md5
+	 * @param int $privilegio indica el perfil de permisos
+	 * @return mixed usuarioClass object y en caso de falla un FALSE
+	 */
+	function login($mail,$pass){
+		//Cargar clase usuario
+		require('loginClass.php');
+		
+		//Conectarse a la base de datos
+		require('dbdata.inc');
+		require('dbClass.php');
+		//echo $mail."     ".$pass;
+		$conexion  = new DB($hostdb, $userdb, $passdb, $db);
+		 
+		if(!$conexion -> conecta())
+			die('No se ha podido realizar la conexion a la bd');
+
+
+		//Limpiar las variables recibidas
+			$mail		=  $conexion->limpiarVariable($mail);
+			$pass		=  $conexion->limpiarVariable($pass);
+			
+
+		//Crear el query
+		$query = "SELECT mail, pass  FROM usuario 
+				  WHERE
+				  	mail= '$mail'
+				  	AND
+				  	pass= '$pass'"; 
+				  
+				  
+
+		//Ejecutar el query
+		//echo $query;
+		$resultado = $conexion -> ejecutarConsulta($query);
+		$conexion->cerrar();
+		if($resultado == FALSE){
+			//Cerrar la conexion
+			return FALSE;
+		}
+		
+		
+		return TRUE;
+
+		
+	}
+
+}
+
+?>
---
+++ Model/loginClass.php
@@ -0,0 +1,15 @@
+<?php
+
+class loginClass{
+
+		//atributos	
+		public $mail;
+		public $pass;
+		
+
+	function __construct($mail,$pass){
+		$this->mail 	 = $mail;
+		$this->pass 	 = $pass;
+		
+	}
+}
